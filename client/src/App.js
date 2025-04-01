f910d98a73f56c55646758d6584d762ba6ab5059
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-01 00:08:32 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,12 @@ import Login from './components/Login';
 import Search from './components/Search';
 import Artist from './components/Artist';
 import Albums from './components/Albums';
+import Callback from './components/Callback';
+import AuthSpotify from './components/AuthSpotify';
+import './App.css';
+import './index.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
@@ -13,6 +17,8 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route path="/artist/:id" element={<Artist />} />
         <Route path="/artist/:id/albums" element={<Albums />} />
+        <Route path="/callback" element={<Callback />} />
+        <Route path="/auth/spotify" element={<AuthSpotify />} />
       </Routes>
     </Router>
   );
