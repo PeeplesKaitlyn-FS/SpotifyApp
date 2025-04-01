5558cd096164a36896cfca65c11beb00f37e6fb2
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-01 00:09:03 -0400

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,62 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const SpotifyWebApi = require('spotify-web-api-node');
+const axios = require('axios');
+const dotenv = require('dotenv');
 
-// Get access token from Spotify API
-router.get('/token', (req, res) => {
+dotenv.config();
+
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
+
+router.get('/login', (req, res) => {
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=user-read-private user-read-email`;
+  res.redirect(authUrl);
+});
+
+router.get('/callback', (req, res) => {
   const code = req.query.code;
-  const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: 'http://localhost:3000/callback'
-  });
-  spotifyApi.clientCredentialsFlow().then((data) => {
-    const accessToken = data.body['access_token'];
-    res.json({ accessToken });
-  });
-});
+  const tokenUrl = 'https://accounts.spotify.com/api/token';
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+  const data = {
+    grant_type: 'authorization_code',
+    code,
+    redirect_uri: redirectUri,
+    client_id: clientId,
+    client_secret: clientSecret,
+  };
 
-// Search for artists on Spotify API
-router.get('/search', (req, res) => {
-  const query = req.query.q;
-  const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: 'http://localhost:3000/callback'
-  });
-  spotifyApi.searchArtists(query).then((data) => {
-    const artists = data.body.artists.items;
-    res.json({ artists });
-  });
-});
-
-// Get artist details from Spotify API
-router.get('/artist/:id', (req, res) => {
-  const id = req.params.id;
-  const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: 'http://localhost:3000/callback'
-  });
-  spotifyApi.getArtist(id).then((data) => {
-    const artist = data.body;
-    res.json({ artist });
-  });
-});
-
-// Get artist albums from Spotify API
-router.get('/artist/:id/albums', (req, res) => {
-  const id = req.params.id;
-  const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: 'http://localhost:3000/callback'
-  });
-  spotifyApi.getArtistAlbums(id).then((data) => {
-    const albums = data.body.items;
-    res.json({ albums });
-  });
+  axios.post(tokenUrl, data, { headers })
+    .then((response) => {
+      const accessToken = response.data.access_token;
+      // Use the access token to make API requests to Spotify's Web API
+      res.send(`Access token: ${accessToken}`);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error exchanging code for access token');
+    });
 });
 
 module.exports = router;
