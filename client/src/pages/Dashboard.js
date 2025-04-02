4fd2fe115e641e45517a0ae2fc39f340da3d044a
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-02 03:17:11 -0400

    Create Dashboard.js

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Spotify Dashboard</h1>
+      <p>Welcome to your Spotify dashboard!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
