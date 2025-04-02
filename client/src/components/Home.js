d2b80e47303eafa0e8603393afb181265d8d5758
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-02 17:07:49 -0400

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,7 @@ import React from "react";
 const Home = () => {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
-      <h1>Spotify Authorization Code Flow</h1>
+      <h1>Spotify Web API</h1>
       <a href="http://localhost:3000/login">
         <button style={{ padding: "10px 20px", fontSize: "16px" }}>
           Login with Spotify
