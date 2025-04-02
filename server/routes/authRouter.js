c0e01160beb2064d22770973477ab35617070d15
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-02 01:58:30 -0400

    Deleted Irrelevant Files

--- server/routes/authRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-
-router.get('/spotify', passport.authenticate('spotify', { scope: ['user-read-email', 'user-read-private', 'playlist-read-private'] }));
-
-module.exports = router;
