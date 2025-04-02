2694b994c6c9fd18fc84b50da25307db39df28ee
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-02 03:17:24 -0400

    Update Passport.js

--- server/Passport.js
+++ server/Passport.js
@@ -1,5 +1,18 @@
 const passport = require('passport');
 const SpotifyStrategy = require('passport-spotify').Strategy;
+const dotenv = require('dotenv');
+dotenv.config();
+
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+const CALLBACK_URL = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+
+const passportConfig = {
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  callbackURL: CALLBACK_URL,
+  scope: ['user-read-email', 'user-read-private', 'playlist-read-private'],
+};
 
 passport.use(new SpotifyStrategy({
   clientID: passportConfig.clientID,
