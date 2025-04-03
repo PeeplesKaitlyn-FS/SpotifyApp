a022b458406ae669038746c19565707ee6a30c97
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-03 00:02:46 -0400

    Update DashboardLinks.js

--- client/src/components/DashboardLinks.js
+++ client/src/components/DashboardLinks.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import './App.css';
+ 
 
 const DashboardLinks = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
