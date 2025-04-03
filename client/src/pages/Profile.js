98695457ee9e0c506a5b52c5592214575f6939c8
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-03 14:24:20 -0400

    Update Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom"; 
-import './App.css';
+import '../App.css';
  
 
 const API_BASE_URL = "http://localhost:3000"; 
