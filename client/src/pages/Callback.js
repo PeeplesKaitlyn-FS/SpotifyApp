4e63d66ba500229a209df09acd3503afba4c55bb
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-05 22:49:28 -0400

    Update Callback.js

--- client/src/pages/Callback.js
+++ client/src/pages/Callback.js
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import './App.css';
-
 
 const API_BASE_URL = "http://localhost:3000";
 
@@ -11,21 +9,25 @@ function Callback() {
 
   useEffect(() => {
     const code = new URLSearchParams(window.location.search).get("code");
-    
-    if (code) {
-      axios.get(`${API_BASE_URL}/callback?code=${code}`)
-        .then((response) => {
-          const { access_token, refresh_token } = response.data;
-          
-          localStorage.setItem("accessToken", access_token);
-          localStorage.setItem("refreshToken", refresh_token);
-
-          navigate("/dashboard");
-        })
-        .catch((error) => {
-          console.error("Error fetching token:", error);
-        });
+    if (!code) {
+      console.error("No authorization code found in URL");
+      navigate("/");
+      return;
     }
+
+    axios.get(`${API_BASE_URL}/callback?code=${code}`)
+      .then((response) => {
+        const { access_token, refresh_token } = response.data;
+
+        localStorage.setItem("accessToken", access_token);
+        localStorage.setItem("refreshToken", refresh_token);
+
+        navigate("/dashboard");
+      })
+      .catch((error) => {
+        console.error("Error fetching token:", error);
+        navigate("/");
+      });
   }, [navigate]);
 
   return <div>Loading...</div>;
