82e7345ea578ff179b6f92ac6bac8d21edef7886
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-04-06 00:05:24 -0400

    Update Dashboard.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
-import DashboardLinks from "../components/DashboardLinks";  
+import DashboardLinks from "../components/DashboardLinks";
+import Search from "../components/Search";
 
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <div className="dashboard-content">
+        <Search />
         <h1>Spotify Dashboard</h1>
         <p>Welcome to your personalized Spotify dashboard!</p>
         <DashboardLinks />
